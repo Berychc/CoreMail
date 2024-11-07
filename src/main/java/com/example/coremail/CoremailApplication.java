b848053bdc7282fce2e1165bd0913d8cb2b3967d
Berychc <treewincsgo@gmail.com>
2024-11-07 11:12:13 +0300

    Start project MAILCORE

---
+++ src/main/java/com/example/coremail/CoremailApplication.java
@@ -0,0 +1,13 @@
+package com.example.coremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoremailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoremailApplication.class, args);
+	}
+
+}
